57d774a44a42479fe67e9a95ed2be4cbb733bf89
rasyid annas <rasshit.dsgn@gmail.com>
2023-10-22 06:02:21 +0700

    003. All about routing.mp4

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//this is for 404 page
+Route::fallback(function () {
+    return "Still got somewhere";
+});
