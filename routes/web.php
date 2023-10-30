0ede707b0796aea1dfb425c1deb25d5260d351ae
rasyid annas <rasshit.dsgn@gmail.com>
2023-10-30 09:13:48 +0700

    012. Forms and CSRF Protection.mp4

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
@@ -24,9 +25,17 @@ Route::get('/tasks', function () {
     ]);
 })->name('tasks.index');
 
+//this is for show crete content
+Route::view('/tasks/create', 'create')
+    ->name('tasks.create');
+
 Route::get('/tasks/{id}', function ($id) {
     return view('show', ['task' => App\Models\Task::findOrFail($id)]);
 })->name('tasks.show');
+
+Route::post('/tasks', function (Request $request) {
+    dd($request->all());
+})->name('tasks.store');
 
 //this is for 404 page
 Route::fallback(function () {
