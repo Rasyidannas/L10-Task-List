0459e4d402fd054dff1a3035e51c0c02599c4166
rasyid annas <rasshit.dsgn@gmail.com>
2023-10-30 10:46:40 +0700

    014. Sessions, Errors & Flash Message.mp4

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,11 +2,16 @@
 <html>
    <head>
       <title>Laravel 10 task List App</title>
+      @yield('styles')
    </head>
 
    <body>
       <h1>@yield('title')</h1>
       <div>
+         {{-- This is for dicplay session --}}
+         @if(session()->has('success'))
+            <div>{{ session('success') }}</div>
+         @endif
          @yield('content')
       </div>
    </body>
